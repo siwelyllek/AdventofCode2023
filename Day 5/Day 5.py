938ca99a29d6351fd9bc79c2afa210cb562bb925
Lewis Kelly <lewiskelly2.5@gmail.com>
2024-01-14 14:09:36 +1000

    part 1 setup

--- Day 5/Day 5.py
+++ Day 5/Day 5.py
@@ -1,1 +1,12 @@
 # the main question on day 5 is: What is the lowest location number that corresponds to any of the initial seed numbers?
+
+import re
+
+with open("./Day 5/input-test.txt", "r") as f:
+    input = f.read()
+
+def part1(input):
+    print(input)
+
+
+print('Lowest location:', part1(input))
