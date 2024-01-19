337206c33d2b4357b9c2d702386b2cbbbaef86f6
Lewis Kelly <lewiskelly2.5@gmail.com>
2024-01-20 00:53:50 +1000

    Onto Day 5 part 2 but performance issues

--- Day 5/Day 5 - Part 2.py
+++ Day 5/Day 5 - Part 2.py
@@ -7,14 +7,17 @@ with open("./Day 5/input.txt", "r") as f:
 
 # seeds are the numbers that are in the first line of the input
 def extract_seeds(input):
-    seeds = []
-
     # for firstline only
     firstline = input.split("\n\n")[0]
 
-    # find all numbers which can be more than one digit and are seperated by whitespace in the first line and add them to the seeds list
-    seeds = re.findall(r'\d+', firstline)
-    return seeds
+    # find all numbers which can be more than one digit and are separated by whitespace in the first line and add them to the seeds list
+    seeds = [int(seed) for seed in re.findall(r'\d+', firstline)]
+
+
+    # Generate the final seeds list using a list comprehension
+    final_seeds = (j for i in range(0, len(seeds), 2) for j in range(seeds[i], seeds[i] + seeds[i+1]))
+
+    return final_seeds
     
 
 def extract_maps(input):
@@ -22,19 +25,19 @@ def extract_maps(input):
     return maps
 
         
-def part1(input):
+def part2(input):
     maps = extract_maps(input)
     # print(segments)
-    seeds = extract_seeds(input)
+    seeds = set(extract_seeds(input))
     # print(seeds)
 
     min_location = float('inf')
-    for x in map(int, seeds):
+    for x in seeds:
         for seg in maps[1:]:
             for conversion in re.findall(r'(\d+) (\d+) (\d+)', seg):
                 destination, start, delta = map(int, conversion)
-                print(destination, start, delta)
-                if x in range(start, start + delta):
+                # print(destination, start, delta)
+                if start <= x < start + delta:
                     x += destination - start
                     break
 
@@ -44,4 +47,4 @@ def part1(input):
 
 
 
-print('Lowest location:', part1(input))
+print('Lowest location:', part2(input))
